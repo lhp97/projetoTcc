3d864b056051b0e35c9b51dc51ef44b7b5e7e3a1
Luis Prado <lhdprado@gmail.com>
2020-07-01 19:51:25 -0300

    Projeto Criado

---
+++ src/main/java/com/lhprado/projetotcc/ProjetotccApplication.java
@@ -0,0 +1,13 @@
+package com.lhprado.projetotcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetotccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetotccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lhprado/projetotcc/ProjetotccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhprado.projetotcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetotccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
